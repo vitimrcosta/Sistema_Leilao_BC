4d219286cdf9fda305f3fbe2250110b4985c60ae
vitimrcosta <victor.rcosta@outlook.com>
2025-06-03 10:57:09 -0300

    Modificando o email_service.py

--- services/email_service.py
+++ services/email_service.py
@@ -1,7 +1,9 @@
 class EmailService:
+    #Método estático
+    #É independente do estado da classe ou do objeto (não usa atributos da classe ou instância).
+    # Pode ser chamado diretamente pela classe, sem precisar criar um objeto.
     @staticmethod
     def enviar(destinatario: str, assunto: str, mensagem: str):
         print(f"📧 E-mail enviado para {destinatario}:")
         print(f"Assunto: {assunto}")
         print(f"Mensagem: {mensagem}\n")
-        # Em produção, substituir por um serviço real (SMTP, SendGrid, etc.)
--- tests/test_email_service.py
+++ tests/test_email_service.py
@@ -1,6 +1,4 @@
 from services.email_service import EmailService
-from io import StringIO
-import sys
 
 def test_envio_email(capsys):
     EmailService.enviar("comprador@email.com", "Teste", "Mensagem de teste")
